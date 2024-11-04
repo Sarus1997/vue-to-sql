97efa4f3d75b8b52b01481de56485de19277d220
Sarus1997 <saharat.rus@gmail.com>
2024-11-04 21:29:04 +0700

    update

--- src/views/CatPage.vue
+++ src/views/CatPage.vue
@@ -9,12 +9,18 @@
           </v-card-title>
           <v-card-text>
             <p class="body-1">ค้นหาด้วยตัวเลข เช่น 100 - 599</p>
-            <v-text-field v-model="statusCode" label="Enter Status Code" type="number"
-              @keyup.enter="fetchStatusImage" />
-            <v-btn color="primary" @click="fetchStatusImage" class="mt-2">
-              Fetch Image
+            <v-text-field v-model="statusCode" label="Enter Status Code" type="number" :disabled="isLoading"
+              @keyup.enter="onButtonClick" />
+            <v-btn :class="{ 'scale-animation': isButtonClicked }" color="primary" @click="onButtonClick" class="mt-2"
+              :disabled="isLoading">
+              <template v-if="isLoading">
+                <v-progress-circular indeterminate size="20" color="white" />
+              </template>
+              <template v-else>
+                Fetch Image
+              </template>
             </v-btn>
-            <div v-if="imageUrl" class="mt-4">
+            <div v-if="imageUrl && !isLoading" class="mt-4">
               <img :src="imageUrl" alt="HTTP Status Image" class="img-responsive" />
             </div>
           </v-card-text>
@@ -34,11 +40,25 @@ import { ref } from 'vue';
 
 const statusCode = ref('');
 const imageUrl = ref(null);
+const isLoading = ref(false);
+const isButtonClicked = ref(false);
 
-function fetchStatusImage() {
+async function fetchStatusImage() {
+  isLoading.value = true;
+  imageUrl.value = null;
+  await new Promise(resolve => setTimeout(resolve, 1000));
   if (statusCode.value) {
     imageUrl.value = `https://http.cat/${statusCode.value}`;
   }
+  isLoading.value = false;
+}
+
+function onButtonClick() {
+  isButtonClicked.value = true;
+  fetchStatusImage();
+  setTimeout(() => {
+    isButtonClicked.value = false;
+  }, 300);
 }
 </script>
 
@@ -46,5 +66,23 @@ function fetchStatusImage() {
 .img-responsive {
   max-width: 100%;
   height: auto;
+}
+
+.scale-animation {
+  animation: scale-up 0.3s ease;
+}
+
+@keyframes scale-up {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.1);
+  }
+
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
